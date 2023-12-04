11e8443ff4e434f9f006ae491af2f32e637f464a
johnk1917 <96326315+johnk1917@users.noreply.github.com>
2023-12-04 19:02:23 +0000

    Update spotifyd.nix

--- home-manager/modules/spotifyd/spotifyd.nix
+++ home-manager/modules/spotifyd/spotifyd.nix
@@ -2,10 +2,10 @@
 xdg.configFile."/home/john1917/.config/spotifyd/spotifyd.conf".text = ''
   [global]
   # Your Spotify account name.
-  username = "johnfkennedy04@gmail.com"
+  username = "no@no.com"
 
   # Your Spotify account password.
-  password = "Jfk03232004"
+  password = "nononono"
 
   # backend
   backend = "pulseaudio"
